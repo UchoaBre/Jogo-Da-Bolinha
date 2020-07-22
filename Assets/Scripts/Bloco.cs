7716a5941caffbaedcc559d500708536abd5bf33
UchoaBre <brendo-uchoa@outlook.com>
2020-07-22 20:32:43 -0300

    first commit

---
+++ Assets/Scripts/Bloco.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloco : MonoBehaviour
+{
+    public GameObject somDoBloco;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindObjectOfType<GameManager>().AumentarQuantidadeDeBlocos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Bola"))
+        {
+            FindObjectOfType<GameManager>().DiminuirQuantidadeDeBlocos();
+            Instantiate(somDoBloco, transform.position, transform.rotation);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public int quantidadeDeBlocos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            SairDoJogo();
+        }
+    }
+
+    public void AumentarQuantidadeDeBlocos()
+    {
+        quantidadeDeBlocos += 1;
+    }
+
+    public void DiminuirQuantidadeDeBlocos()
+    {
+        quantidadeDeBlocos -= 1;
+
+        if(quantidadeDeBlocos <= 0)
+        {
+            if(SceneManager.GetActiveScene().buildIndex + 1 < SceneManager.sceneCountInBuildSettings)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+            else
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+
+    private void SairDoJogo()
+    {
+        Application.Quit();
+    }
+
+}
